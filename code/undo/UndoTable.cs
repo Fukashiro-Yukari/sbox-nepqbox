587665aa667c552a50390c975ed441ad2ec7fd64
Neptune QTG <Neptuneqtg@gmail.com>
2021-09-04 02:49:53 +0800

    Minor changes

---
+++ code/undo/UndoTable.cs
@@ -0,0 +1,119 @@
+using System;
+using System.Collections.Generic;
+
+namespace Sandbox
+{
+	public struct UndoTable
+	{
+		string Name;
+		List<UndoRemove> Entities;
+		Client Owner;
+
+		public UndoTable( string name )
+		{
+			if ( name == null )
+				throw new ArgumentNullException( "UndoTable name cannot be empty" );
+
+			Name = name;
+			Entities = new();
+			Owner = null;
+		}
+
+		private UndoRemove CreateUndoRemove( object obj )
+		{
+			var ent = obj as Entity;
+			var phyjoint = obj as IPhysicsJoint;
+			var particles = obj as Particles;
+
+			if ( ent != null )
+			{
+				return new EntityUndo( ent );
+			}
+			else if ( phyjoint != null )
+			{
+				return new PhysicsJointUndo( phyjoint );
+			}
+			else if ( particles != null )
+			{
+				return new ParticlesUndo( particles );
+			}
+
+			return null;
+		}
+
+		public void Tick()
+		{
+			if ( Entities.Count > 0 )
+			{
+				for ( int i = 0; i < Entities.Count; i++ )
+				{
+					var undo = Entities[i];
+
+					if ( !undo.IsValid() )
+					{
+						var slot = Entities.IndexOf( undo );
+
+						Entities.RemoveAt( slot );
+					}
+				}
+			}
+		}
+
+		public void AddEntity( object obj )
+		{
+			if ( obj == null ) return;
+
+			Entities.Add( CreateUndoRemove( obj ) );
+		}
+
+		public void Add( UndoRemove undo )
+		{
+			Entities.Add( undo );
+		}
+
+		public void Delete()
+		{
+			foreach ( var undoremove in Entities )
+			{
+				undoremove.Delete();
+			}
+		}
+
+		public bool ReplaceEntity( object from, object to )
+		{
+			var isreplace = false;
+
+			foreach ( var undo in Entities )
+			{
+				if ( undo.ObjectEquals( from ) )
+				{
+					isreplace = true;
+
+					undo.Replace( to );
+				}
+			}
+
+			return isreplace;
+		}
+
+		public bool IsValid()
+		{
+			return Entities.Count > 0;
+		}
+
+		public void SetClient( Client client )
+		{
+			Owner = client;
+		}
+
+		public Client GetClient()
+		{
+			return Owner;
+		}
+
+		public string GetName()
+		{
+			return Name;
+		}
+	}
+}
